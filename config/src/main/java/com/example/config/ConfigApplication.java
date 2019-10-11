06c1ca9a6a419d18d47088210c4b3fc9f0aadecd
jzanetii4 <hangqiy@163.com>
2019-10-12 04:26:40 +1100

    springcloud自动更新

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/***
+ * 该模块作为config-server
+ *
+ */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
